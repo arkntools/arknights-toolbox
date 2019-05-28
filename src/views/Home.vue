ecceefeb7d335c5dc5504af0666b9d8066b419fc
神代綺凜 <i@lolico.moe>
2019-05-28 17:46:44 +0800

    针对移动设备优化【公开招募计算】

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,9 +5,9 @@
 			<p>Github: <a href="https://github.com/Tsuk1ko/arknights-toolbox" target="_blank">Tsuk1ko/arknights-toolbox</a></p>
 			<p>开发中……</p>
 			<p>如果有好的想法和建议或希望增加的功能欢迎到项目中提 issue</p>
-			<h2>计划功能</h2>
+			<h2>主要计划功能</h2>
 			<ol>
-				<li>公开招募计算【已完成】【改进中】</li>
+				<li>公开招募计算【已完成】【针对移动设备优化】</li>
 				<li>材料合成计算</li>
 			</ol>
 			<h2>数据来源</h2>

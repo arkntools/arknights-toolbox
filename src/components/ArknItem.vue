16f0f49ae1daf7d7304a19c7ac640a837cc3028c
神代綺凜 <i@lolico.moe>
2019-06-11 04:34:08 +0800

    [add] 干员升级计算

---
+++ src/components/ArknItem.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="no-sl mdui-m-r-1" :t="t">
+		<div class="mdui-valign" style="height: 100%;">
+			<img class="no-pe item-img" :src="`/img/${img}.png`" />
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'arkn-item',
+	props: {
+		t: [Number, String],
+		img: String
+	}
+}
+</script>
+
+<style scoped>
+.item-img {
+	max-width: 100%;
+	max-height: 100%;
+}
+</style>
--- src/components/_MduiSelectNum.vue
+++ src/components/_MduiSelectNum.vue
@@ -11,7 +11,7 @@ export default {
 		event: 'change'
 	},
 	props: {
-		value: Number,
+		value: [Number, String],
 		options: Array
 	}
 }

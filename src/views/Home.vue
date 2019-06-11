5cea486a842749da00a192ab3a4b4908912df6e4
神代綺凜 <i@lolico.moe>
2019-06-12 03:45:01 +0800

    [add] service worker

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,7 @@
 			<h1>明日方舟工具箱</h1>
 			<p>Github: <a href="https://github.com/Tsuk1ko/arknights-toolbox" target="_blank">Tsuk1ko/arknights-toolbox</a></p>
 			<p>开发中……宗旨是简洁美观且对移动设备友好</p>
+			<p>本应用使用 Service Worker 缓存加载过的图片以及应用本身，因此甚至可以离线使用</p>
 			<p>如果有好的想法、建议、希望增加的功能，以及 BUG，欢迎到项目中提 <a href="https://github.com/Tsuk1ko/arknights-toolbox/issues" target="_blank">issue</a></p>
 			<h2>主要计划功能</h2>
 			<ol>

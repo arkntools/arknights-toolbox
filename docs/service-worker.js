1fb1b505112678bb13026c7e6b11e394ae9c9c15
神代綺凜 <i@lolico.moe>
2019-06-13 21:02:35 +0800

    test

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -27,3 +27,5 @@ workbox.core.setCacheNameDetails({prefix: "arknights-toolbox"});
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+//test

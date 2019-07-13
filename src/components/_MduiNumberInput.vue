4b2705a342a170b68553fa7a5ab32b327e9411ca
神代綺凜 <i@lolico.moe>
2019-07-14 03:10:26 +0800

    [add] cost-effective calculation of stages

--- src/components/_MduiNumberInput.vue
+++ src/components/_MduiNumberInput.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="mdui-textfield mdui-p-y-0">
-		<label class="mdui-textfield-label">
+		<label class="mdui-textfield-label no-sl">
 			<slot></slot>
 		</label>
 		<input class="mdui-textfield-input mdui-p-y-0" type="number" :value="value" @input="$emit('input', $event.target.value)" min="0" step="1" />

73e9cfbb8df9d7fa88185051f7288cd2dd1e0a1a
神代綺凜 <i@loli.best>
2019-10-30 16:05:22 +0800

    Update Base.vue

--- src/views/Base.vue
+++ src/views/Base.vue
@@ -1,53 +1,62 @@
 <template>
     <div id="arkn-base">
         <!-- 标签面板 -->
-        <div class="mdui-row">
-            <div class="mdui-col-xs-12">
-                <table class="mdui-table tag-table">
-                    <tbody>
-                        <tr v-for="(tags, type) in tag" :key="type">
-                            <td width="1">
-                                <button class="mdui-btn mdui-btn-dense mdui-color-teal no-pe tag-btn">{{buildingsShort[type]}}</button>
-                            </td>
-                            <td>
-                                <tag-button v-for="tag in tags" :key="tag" v-model="selected[type][tag]" :notSelectedColor="color.notSelected" :selectedColor="color.selected">{{tag}}</tag-button>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+        <div id="drawer" :class="$root.smallScreen?'mdui-drawer mdui-drawer-right mdui-drawer-close':false">
+            <div class="mdui-row">
+                <div class="mdui-col-xs-12 tag-group-outside" v-for="(tagTypeGroup, index) in tagDisplay" :key="index">
+                    <div class="tag-group" v-for="tagType of tagTypeGroup" :key="tagType">
+                        <label class="mdui-textfield-label" :style="{ color: color[tagType] ? `var(--${color[tagType]})` : false }">{{tagType}}</label>
+                        <tag-button v-for="tagName in tag[tagType]" :key="tagName" v-model="selected[tagType][tagName]" :notSelectedColor="`${color[tagType] || color.selected} opacity-5`" :selectedColor="color[tagType] || color.selected">{{tagName}}</tag-button>
+                    </div>
+                </div>
+            </div>
+            <div class="mdui-row mdui-m-t-2">
+                <div class="mdui-col-xs-12">
+                    <button class="mdui-btn mdui-ripple mdui-btn-dense mdui-color-red tag-btn" @click="reset">重置</button>
+                    <mdui-switch class="mdui-m-l-2" v-for="(zh, en) in settingZh" :key="en" v-model="setting[en]">{{zh}}</mdui-switch>
+                </div>
             </div>
         </div>
-        <div class="mdui-row mdui-m-t-4">
+        <!-- 技能列表 -->
+        <div :class="`mdui-row ${$root.smallScreen?'':'mdui-m-t-4'}`">
             <div class="mdui-col-xs-12">
-                <table class="mdui-table">
-                    <thead>
-                        <tr>
-                            <th colspan="2" class="mdui-text-center">干员</th>
-                            <th class="mdui-text-center">解锁条件</th>
-                            <th class="mdui-text-center">设施</th>
-                            <th class="mdui-text-center">技能</th>
-                            <th>效果</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <template v-for="item of display">
-                            <tr v-for="(skill, index) in item.skills" :key="index">
-                                <td :rowspan="item.skills.length" v-if="index===0">
-                                    <img class="mdui-card-header-avatar" :src="addition[item.name]?$root.avatar(addition[item.name]):false" />
-                                </td>
-                                <td v-else class="hidden"></td>
-                                <td nowrap :rowspan="item.skills.length" v-if="index===0">{{item.name}}</td>
-                                <td v-else class="hidden"></td>
-                                <td nowrap class="mdui-text-center">{{skill.unlock}}</td>
-                                <td nowrap class="mdui-text-center">{{skill.building}}</td>
-                                <td nowrap class="mdui-text-center"><span :class="`skill-card ${color[skill.building]}`">{{skill.name}}</span></td>
-                                <td v-html="skill.display.description"></td>
+                <div class="mdui-table-fluid">
+                    <table class="mdui-table" id="skill-table">
+                        <thead>
+                            <tr>
+                                <th colspan="2" class="mdui-text-center mdui-hidden-xs-down">干员</th>
+                                <th class="mdui-text-center mdui-hidden-sm-up">干员</th>
+                                <th class="mdui-text-center">解锁</th>
+                                <th class="mdui-text-center mdui-hidden-sm-down">设施</th>
+                                <th class="mdui-text-center">技能</th>
+                                <th>效果</th>
                             </tr>
-                        </template>
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            <template v-for="(item, itemIndex) of display">
+                                <tr v-for="(skill, skillIndex) in item.skills" :key="`${itemIndex}-${skillIndex}`">
+                                    <td :rowspan="item.skills.length" v-if="skillIndex===0" class="mdui-hidden-xs-down">
+                                        <img class="mdui-card-header-avatar" :src="addition[item.name]?$root.avatar(addition[item.name]):false" />
+                                    </td>
+                                    <td v-else class="hidden"></td>
+                                    <template v-if="skillIndex===0">
+                                        <td nowrap :rowspan="item.skills.length" class="mdui-hidden-xs-down">{{item.name}}</td>
+                                        <td nowrap :rowspan="item.skills.length" class="mdui-text-center mdui-hidden-sm-up">{{item.name}}</td>
+                                    </template>
+                                    <td v-else class="hidden"></td>
+                                    <td nowrap class="mdui-text-center">{{skill.display.unlock}}</td>
+                                    <td nowrap class="mdui-text-center mdui-hidden-sm-down">{{skill.building}}</td>
+                                    <td nowrap class="mdui-text-center"><span :class="`skill-card ${color[skill.building]}`">{{skill.name}}</span></td>
+                                    <td :nowrap="$root.smallScreen" v-html="skill.display.description"></td>
+                                </tr>
+                            </template>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
+        <!-- 浮动按钮 -->
+        <button v-if="$root.smallScreen" class="mdui-fab mdui-fab-fixed mdui-fab-mini mdui-color-pink-accent mdui-ripple" @click="drawer?null:drawer=new $root.Mdui.Drawer('#drawer');drawer.toggle()" style="z-index:10000"><i class="mdui-icon material-icons">sort</i></button>
     </div>
 </template>
 
@@ -73,18 +82,6 @@ const color = {
 };
 
 const buildings = ['制造站', '贸易站', '发电站', '控制中枢', '宿舍', '会客室', '加工站', '训练室', '人力办公室'];
-const buildingsShort = {
-    基建设施: '设施',
-    制造站: '制造',
-    贸易站: '贸易',
-    发电站: '电厂',
-    控制中枢: '中枢',
-    宿舍: '宿舍',
-    会客室: '会客',
-    加工站: '加工',
-    训练室: '训练',
-    人力办公室: '人力',
-};
 
 const keyword = {
     制造站: {
@@ -93,12 +90,11 @@ const keyword = {
         作战记录: /作战记录/,
         源石: /源石/,
         仓库容量: /仓库容量上限\+/,
-        心情消耗: /心情(每小时)?消耗-/,
+        // 心情消耗: /心情(每小时)?消耗-/,
     },
     贸易站: {
         订单效率: /订单(获取)?效率\+/,
-        订单上限: /订单上限\+/,
-        心情消耗: /心情(每小时)?消耗-/,
+        // 订单上限: /订单上限\+/,
     },
     控制中枢: {
         订单效率: /订单(获取)?效率\+/,
@@ -138,11 +134,21 @@ const keyword = {
     },
 };
 
+const tagDisplay = [['基建设施'], ['制造站', '贸易站', '控制中枢', '宿舍', '会客室', '加工站', '训练室']];
+
 const skillHightlight = html =>
     html
         .replace(/(?<!消耗)((提升)|(\+))([\d.]+%?)/g, '$2<span class="mdui-text-color-blue">$3$4</span>')
         .replace(/(?<!消耗)-[\d.]+%?(?!心情消耗)|(?<=消耗)\+[\d.]+%?/g, '<span class="mdui-text-color-red">$&</span>')
         .replace(/(?<=消耗)-[\d.]+%?|-[\d.]+%?(?=心情消耗)|(?<![+-\d.])[\d.]+%?|(?<=每个).{1,8}(?=干员)|(?<=更容易获得).*?(?=线索)|(?<=与).*?(?=在同一个)|(贵金属|作战记录|源石)(?=类)|(?<=目标是).*?(?=，则)|(?<=，).*?(?=干员的?专精)/g, '<span class="mdui-text-color-blue">$&</span>');
+
+const unlockShort = {
+    初始携带: '-',
+    等级30: '30级',
+    等级30级: '30级',
+    精英化1: '精1',
+    精英化2: '精2',
+};
 
 export default {
     name: 'arkn-base',
@@ -168,13 +174,14 @@ export default {
             ),
             color,
             buildings,
-            buildingsShort,
+            tagDisplay,
             setting: {
                 hideIrrelevant: false,
             },
             settingZh: {
                 hideIrrelevant: '隐藏同一干员与筛选无关的技能',
             },
+            drawer: null,
         };
         data.base = _.transform(
             data.baseTable,
@@ -182,6 +189,7 @@ export default {
                 skills.forEach(skill => {
                     skill.display = {
                         description: skillHightlight(skill.description),
+                        unlock: unlockShort[skill.unlock] || skill.unlock,
                     };
                 });
                 arr.push({ name, skills });
@@ -234,17 +242,49 @@ export default {
         display() {
             const need = _.transform(
                 this.selected,
-                (arr, tags, key) => {
+                (arr, tags, type) => {
                     _.each(tags, (isSelect, tag) => {
-                        if (isSelect) arr.push(this.category[key][tag]);
+                        if (isSelect) arr.push(this.category[type][tag]);
                     });
                 },
                 []
             );
-            return need.length > 0 ? _.union(...need) : this.base;
+            if (_.isEmpty(need)) return this.base;
+            let result = _.union(...need);
+            if (this.setting.hideIrrelevant) {
+                const { regs, buildings } = _.transform(
+                    this.selected,
+                    ({ regs, buildings }, tags, type) => {
+                        _.each(tags, (isSelect, tag) => {
+                            if (isSelect) {
+                                if (type === '基建设施') buildings.push(tag);
+                                else regs.push(keyword[type][tag]);
+                            }
+                        });
+                    },
+                    { regs: [], buildings: [] }
+                );
+                result = _.cloneDeep(result);
+                _.each(result, item => {
+                    item.skills = _.transform(
+                        item.skills,
+                        (arr, skill) => {
+                            const c1 = !_.isEmpty(regs) && regs.some(reg => reg.test(skill.description));
+                            const c2 = !_.isEmpty(buildings) && buildings.includes(skill.building);
+                            if (c1 || c2) arr.push(skill);
+                        },
+                        []
+                    );
+                });
+            }
+            return result;
         },
     },
-    methods: {},
+    methods: {
+        reset() {
+            this.selected = _.mapValues(this.selected, group => _.mapValues(group, () => false));
+        },
+    },
     created() {
         const setting = localStorage.getItem('base.setting');
         if (setting) this.setting = JSON.parse(setting);
@@ -253,10 +293,54 @@ export default {
 </script>
 
 <style>
+:root {
+    --mdui-color-amber-400: #ffca28;
+    --mdui-color-light-blue-700: #0288d1;
+    --mdui-color-green-600: #43a047;
+    --mdui-color-green-900: #1b5e20;
+    --mdui-color-cyan-300: #4dd0e1;
+    --mdui-color-orange-900: #e65100;
+    --mdui-color-lime-400: #d4e157;
+    --mdui-color-red-900: #b71c1c;
+    --mdui-color-grey-700: #616161;
+}
 .hidden {
     display: none !important;
 }
 .skill-card {
     padding: 4px;
+    font-size: 12px;
+}
+.tag-group-outside {
+    white-space: normal;
+    padding-right: 4px;
+}
+.tag-group {
+    display: inline-block;
+    padding: 4px 0;
+    margin-right: 4px;
+    white-space: normal;
+}
+.mdui-btn.tag-btn:hover {
+    opacity: 0.8;
+}
+#skill-table td,
+#skill-table th {
+    padding: 8px 8px;
+}
+#skill-table td:first-child,
+#skill-table th:first-child {
+    padding-right: 0;
+    padding-left: 16px;
+}
+#skill-table td:last-child,
+#skill-table th:last-child {
+    padding-right: 16px;
+}
+#drawer {
+    min-width: 290px;
+}
+#drawer.mdui-drawer {
+    padding: 8px;
 }
 </style>

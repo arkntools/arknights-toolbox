5f2afbc80c57cdd0e74fef8a175cb7c82698b19c
神代綺凜 <i@lolico.moe>
2019-11-20 01:47:20 +0800

    [enhance] fault-tolerant

---
+++ tools/updateMaterialPinyin.js
@@ -0,0 +1,19 @@
+const pinyin = require('pinyin');
+const Fse = require('fs-extra');
+const Path = require('path');
+const _ = require('lodash');
+
+const JSON_MATERIAL = Path.join(__dirname, '../src/data/material.json');
+
+const data = Fse.readJSONSync(JSON_MATERIAL);
+data.forEach(item => {
+    const fullPY = pinyin(item.name, {
+        style: pinyin.STYLE_NORMAL,
+        segment: true,
+    });
+    item.pinyin = _.flatten(fullPY)
+        .join('')
+        .toLowerCase();
+});
+
+Fse.writeJSONSync(JSON_MATERIAL, data);

4fe4dc9da07cc1ca49ceb7402e169faa5057ba10
神代綺凜 <i@loli.best>
2019-12-26 02:01:02 +0800

    [update] new material

--- tools/updateMaterialInfo.js
+++ tools/updateMaterialInfo.js
@@ -4,8 +4,13 @@ const Path = require('path');
 const _ = require('lodash');
 
 const JSON_MATERIAL = Path.join(__dirname, '../src/data/material.json');
+const JSON_MATERIAL_ORDER = Path.join(__dirname, '../src/data/materialOrder.json');
 
 const data = Fse.readJSONSync(JSON_MATERIAL);
+const order = _.transform(Fse.readJSONSync(JSON_MATERIAL_ORDER), (o, v, k) => (o[v] = k), {});
+
+data.sort((a, b) => order[a.name] - order[b.name]);
+
 data.forEach(item => {
   const fullPY = pinyin(item.name, {
     style: pinyin.STYLE_NORMAL,

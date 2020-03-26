97612c986fa608e393c5d816ff7d2d22e4c0f35e
神代綺凜 <i@loli.best>
2020-03-26 21:59:34 +0800

    [fix] path error

--- tools/modules/updateTimestamp.js
+++ tools/modules/updateTimestamp.js
@@ -2,6 +2,6 @@ const Fse = require('fs-extra');
 const Path = require('path');
 const dateformat = require('dateformat');
 
-Fse.writeJSONSync(Path.join(__dirname, '../src/data/timestamp.json'), {
+Fse.writeJSONSync(Path.join(__dirname, '../../src/data/timestamp.json'), {
   time: dateformat(new Date(), 'yyyy-m-d'),
 });

9edc865354ce234f8a53bc56062c43238311ce44
神代綺凜 <i@loli.best>
2020-11-22 18:10:44 +0800

    feat: add a setting of "sort by rarity first" in simple mode

--- src/store/material.js
+++ src/store/material.js
@@ -2,13 +2,17 @@ import _ from 'lodash';
 import material from '@/data/item.json';
 
 export const materialTable = _.mapValues(material, (obj, name) => ({ name, ...obj }));
-export const materialList = Object.values(materialTable);
+export const materialList = _.sortBy(Object.values(materialTable), 'sortId');
 export const materials = _.groupBy(materialList, ({ rare }) => rare);
-export const materialOrder = _.sortBy(_.clone(materialList), 'sortId').map(({ name }) => name);
+export const materialOrder = materialList.map(({ name }) => name);
+export const materialRareFirstOrder = _.flatMap(Object.values(materials).reverse(), group =>
+  group.map(({ name }) => name)
+);
 
 export default {
   materials,
   materialTable,
   materialList,
   materialOrder,
+  materialRareFirstOrder,
 };

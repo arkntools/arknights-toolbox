6f9b6d522cb1cd758769388097efa81a95b563fa
神代綺凜 <i@loli.best>
2020-12-27 21:47:40 +0800

    fix: default avatar

--- src/components/_Avatar.vue
+++ src/components/_Avatar.vue
@@ -1,16 +1,21 @@
 <template>
   <img
-    :src="name ? $root.avatar(name) : defaultAvatar"
+    :src="name ? $root.avatar(name) : PNG1P"
     crossorigin="anonymous"
     @error="e => (e.target.src = $root.avatar('no_image'))"
   />
 </template>
 
 <script>
+import { PNG1P } from '@/utils/constant';
+
 export default {
   name: 'avatar',
   props: {
     name: String,
   },
+  data: () => ({
+    PNG1P,
+  }),
 };
 </script>

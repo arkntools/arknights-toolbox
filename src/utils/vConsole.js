ff05df2708b2bca01374acd3498075643511e9f9
神代綺凛 <i@loli.best>
2021-09-21 03:51:10 +0800

    feat: use modified vConsole

--- src/utils/vConsole.js
+++ src/utils/vConsole.js
@@ -3,7 +3,7 @@ export const VConsoleLoaded = () => !!window.VConsole;
 export const loadVConsole = async () => {
   if (VConsoleLoaded()) return;
   await import(
-    /* webpackIgnore: true */ 'https://cdn.jsdelivr.net/npm/vconsole@3.9.1/dist/vconsole.min.js'
+    /* webpackIgnore: true */ 'https://cdn.jsdelivr.net/npm/@arkntools/vconsole@3.9.1/dist/vconsole.min.js'
   );
   new window.VConsole();
 };

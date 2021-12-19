b8b7b31360f3affacfe6847d3a6990dbba4c84f3
神代綺凛 <i@loli.best>
2021-12-19 20:46:52 +0800

    feat: pinyin-pro use customPinyin

--- tools/modules/pinyin.js
+++ tools/modules/pinyin.js
@@ -1,24 +1,18 @@
 const _ = require('lodash');
-const { pinyin } = require('pinyin-pro');
+const { pinyin, customPinyin } = require('pinyin-pro');
 
-const FIXED_LIST = {
+customPinyin({
   薄: 'bo',
-};
+});
 
 const joinPinyin = arr => arr.join('').replace(/ü/g, 'v');
 
 module.exports = words => {
   if (/^[\w\s-]*$/.test(words)) return { full: '', head: '' };
-  const py = pinyin(
-    words
-      .split('')
-      .map(word => FIXED_LIST[word] || word)
-      .join(''),
-    {
-      toneType: 'none',
-      type: 'array',
-    },
-  );
+  const py = pinyin(words, {
+    toneType: 'none',
+    type: 'array',
+  });
   return {
     full: joinPinyin(py),
     head: joinPinyin(_.map(py, 0)),

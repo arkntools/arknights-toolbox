86f16dfb9b28cfa096a4b5222d4e5d5ebb45fd88
神代綺凛 <i@loli.best>
2022-07-06 01:06:40 +0800

    fix: preset uniequip error

--- src/views/Material.vue
+++ src/views/Material.vue
@@ -1905,7 +1905,7 @@ export default {
             });
           });
           uniequip.forEach(({ id, cost }) => {
-            if (!preset.uniequip[id][0]) return;
+            if (!preset.uniequip[id]?.[0]) return;
             for (let i = preset.uniequip[id][1]; i < preset.uniequip[id][2]; i++) {
               _.each(cost[i], (num, m) => {
                 if (!(m in map)) map[m] = new Set();

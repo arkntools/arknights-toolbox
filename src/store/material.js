1228950b61aaf40ff828a59fe8bc5b3b741c73f6
神代綺凛 <i@loli.best>
2022-10-06 17:25:50 +0800

    fix: synthesis cost

--- src/store/material.js
+++ src/store/material.js
@@ -2,6 +2,32 @@ import _ from 'lodash';
 import material from '@/data/item.json';
 import { langList } from './lang';
 
+export const MaterialTypeEnum = {
+  UNKNOWN: -1,
+  MATERIAL: 0,
+  CHIP: 1,
+  MOD_TOKEN: 2,
+  SKILL_BOOK: 3,
+  CHIP_ASS: 4,
+};
+
+const typeAsserts = Object.entries({
+  MATERIAL: id => _.inRange(id, 30011, 32000),
+  CHIP: id => _.inRange(id, 3211, 3300),
+  MOD_TOKEN: id => /^mod_(?:unlock|update)_token/.test(id),
+  SKILL_BOOK: id => _.inRange(id, 3301, 3310),
+  CHIP_ASS: id => String(id) === '32001',
+});
+
+_.each(material, (obj, id) => {
+  const [typeKey] = typeAsserts.find(([, assert]) => assert(id)) || [];
+  if (!typeKey) {
+    obj.type = MaterialTypeEnum.UNKNOWN;
+    return;
+  }
+  obj.type = MaterialTypeEnum[typeKey];
+});
+
 export const materialTable = _.mapValues(material, (obj, name) => ({ name, ...obj }));
 export const materialIdList = Object.keys(materialTable);
 export const materialList = _.sortBy(Object.values(materialTable), 'sortId.cn');
@@ -14,6 +40,7 @@ export const materialRareFirstOrder = _.mapValues(materialOrder, order =>
 );
 
 export default {
+  MaterialTypeEnum,
   materials,
   materialTable,
   materialIdList,

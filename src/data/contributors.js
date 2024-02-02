9161e188ffe4364fc117bc337bf7c3fbcb2c9e30
神代綺凛 <i@loli.best>
2024-02-02 09:18:37 +0800

    chore: update contributors

--- src/data/contributors.js
+++ src/data/contributors.js
@@ -4,6 +4,10 @@ import { contributors, developers, translators } from './contributors.json';
 _.each(contributors, (person, name) => {
   if (!('url' in person)) person.url = `https://github.com/${name}`;
   else if (person.url === null) delete person.url;
+  if (person.avatar.startsWith('gh:')) {
+    const id = person.avatar.replace('gh:', '');
+    person.avatar = `https://avatars.githubusercontent.com/u/${id}?s=32`;
+  }
 });
 
 const mapContributors = list => list.map(name => contributors[name]);

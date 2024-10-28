1006382a624f75220216113463f7efb9fe57afa8
神代綺凛 <i@loli.best>
2024-10-28 18:30:11 +0800

    fix: allow empty text

--- src/utils/ceobeCanteen.js
+++ src/utils/ceobeCanteen.js
@@ -3,8 +3,11 @@ import { computed, inject } from 'vue';
 export const useCeobeApiUtils = () => {
   const isLocaleZH = computed(() => inject('getRoot')?.()?.$root.localeZH);
 
+  /**
+   * @returns {string}
+   */
   const getLocalizedText = obj =>
-    obj[isLocaleZH.value ? 'zh_CN' : 'en_US'] || obj.zh_CN || obj.en_US || '';
+    obj[isLocaleZH.value ? 'zh_CN' : 'en_US'] ?? obj.zh_CN ?? obj.en_US ?? '';
 
   return {
     isLocaleZH,
